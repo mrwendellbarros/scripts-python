f106b01d247a0268a66f2448428a41db4e7dc2f6
Wendell Gabriel Fernandes Barros <102330689+mrwendellbarros@users.noreply.github.com>
2023-01-08 16:04:54 -0300

    Tratando vários valores v1.0

---
+++ desafio059.py
@@ -0,0 +1,31 @@
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+opção = 0
+while opção != 5:
+    print('''    [ 1 ] somar
+    [ 2 ] multiplicar
+    [ 3 ] maior
+    [ 4 ] novos números
+    [ 5 ] sair ddo programa''')
+    opção = int(input('Qual é a su opção? '))
+    if opção == 1:
+        soma = n1 + n2
+        print(f'A soma entre {n1} + {n2} é {soma}')
+    elif opção == 2:
+        produto = n1 * n2
+        print(f'O resultado de {n1} x {n2} é {produto}')
+    elif opção == 3:
+        if n1 > n2:
+            maior = n1
+        else:
+            maior = n2
+        print(f'Entre {n1} e {n2} o maior é {maior}')
+    elif opção == 4:
+        print('Informe os números novamente:')
+        n1 = int(input('Primeiro valor: '))
+        n2 = int(input('Segundo valor: '))
+    elif opção == 5:
+        print('Finalizando...')
+    else:
+        print('Opção inválida. Tente novamente!')
+print('Fim do programa! Volte sempre!')
