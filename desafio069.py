c953c79d581c27f2013f165a19c55ad50cda4139
Wendell Gabriel Fernandes Barros <102330689+mrwendellbarros@users.noreply.github.com>
2023-01-16 03:22:45 -0300

    Análise de dados do grupo

---
+++ desafio069.py
@@ -0,0 +1,21 @@
+tot18 = totM20 = 0
+totH = 0
+while True:
+    idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
+    if idade >= 10:
+        tot18 += 1
+    if sexo == 'M':
+        totH += 1
+    if sexo == 'F' and idade < 20:
+        totM20 += 1
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print(f'Total de pessoas com mais de 18 anos: {tot18}\n'
+      f'Ao todo temos {totH} homens cadastrados\n'
+      f'E temos {totM20} mulheres com menos de 20 anos')
